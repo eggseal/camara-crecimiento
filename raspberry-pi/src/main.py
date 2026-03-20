64f503dd66500ee038d56f17266db503270820df
eggseal <nicos2004@gmail.com>
2026-03-20 15:53:32 -0500

    Add basic Serial communication with ESP32

--- raspberry-pi/src/main.py
+++ raspberry-pi/src/main.py
@@ -2,42 +2,86 @@ import logging
 import threading
 import time
 import json
+from json import JSONDecodeError
 from datetime import datetime
 
-import config
+import config   
 import serial
+import serial.tools.list_ports as list_ports
 from modules.camera import CameraThread
 
 stop_event = threading.Event()
 CameraThread(1, config.STREAM_DEVICE1, stop_event)
 CameraThread(2, config.STREAM_DEVICE2, stop_event)
-modules = (
-    serial.Serial(config.MODULE_DEVICE1, 115200, timeout=5), 
-    serial.Serial(config.MODULE_DEVICE2, 115200, timeout=5)
-)
+module_lock = threading.Lock()
+modules: list[serial.Serial] = []
+
+COMMANDS = {
+    "READ_ALL": lambda: json.dumps({"cmd": 1}).encode() + b"\n"
+}
+
+
+def reconnect_modules():
+    while not stop_event.is_set():
+        ports = list_ports.comports(include_links=True)
+        for port in ports:
+            if port.vid is None: continue
+            with module_lock:
+                if any(mod.port == port.device for mod in modules): continue
+
+            try:
+                logging.info("Attempting serial connection with device %s", port.device)
+                ser = serial.Serial(port.device, 115200, timeout=1)
+                with module_lock:
+                    modules.append(ser)
+                logging.info("Successful serial connection with device %s", port.device)
+            except:
+                logging.error("Failed serial connection with device %s", port.device)
+                pass
+        time.sleep(1)
 
 
 def main():
-    timestamp = datetime.now()
-    for module in modules:
-        module.reset_input_buffer()
-        module.write(json.dumps({"cmd": 1}).encode() + b"\n")
-        module.flush()
-        
-        res = json.loads(module.readline().decode().strip())
-        if res == b"":
-            logging.error("Timed out waiting for ESP32 response.")
-            continue
-        res["timestamp"] = timestamp
-        logging.info(res)
+    timestamp = time.time_ns()
+    with module_lock:
+        for module in modules[:]:
+            try:
+                module.reset_input_buffer()
+                module.reset_output_buffer()
+                module.write(COMMANDS["READ_ALL"]())
+                module.flush()
+                logging.info("Sent command: %s", COMMANDS["READ_ALL"]())
+
+                if module.in_waiting > 0:
+                    res = module.readline()
+                else:
+                    modules.remove(module)
+                    logging.error("Timed out waiting for ESP32 response.")
+                    continue
+                
+                logging.info("Received message %s", res)
+                data = json.loads(res.decode().strip())
+                data["timestamp"] = timestamp
+                logging.info(data)
+            except serial.SerialException as e:
+                logging.error("Serial error: %s", e)
+            except JSONDecodeError as e:
+                logging.error("Error decoding response from %s: %s", module.port, e)
+                module.flush()
+            except Exception as e:
+                logging.error(e)
+    time.sleep(1)
 
 
 if __name__ == "__main__":
+    reconnect_modules_thread = threading.Thread(target=reconnect_modules, daemon=True)
     try:
+        reconnect_modules_thread.start()
         while True:
             main()
-            time.sleep(0.1)
     except KeyboardInterrupt:
         logging.info("Exiting the program...")
     finally:
         stop_event.set()
+        reconnect_modules_thread.join()
+
