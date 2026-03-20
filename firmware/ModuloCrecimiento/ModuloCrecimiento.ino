7c6fc43be4ee9d7e2b4a2c21ebd199358af1da5c
eggseal <nicos2004@gmail.com>
2026-03-20 15:51:39 -0500

    Add basic Serial communication

--- firmware/ModuloCrecimiento/ModuloCrecimiento.ino
+++ firmware/ModuloCrecimiento/ModuloCrecimiento.ino
@@ -1,16 +1,50 @@
+// Libraries
 #include <Preferences.h>
+#include <ArduinoJson.h>
 
-Preferences preferences;
+// Constants
+#define SerialX Serial
 
+// Variables
 uint8_t moduleID = 0;
+
+// Library initialization
+Preferences preferences;
+JsonDocument data;
+
+// Subroutines
+void serialEvent() {
+    if (!SerialX.available()) return;
+    data.clear();
+    DeserializationError error = deserializeJson(data, SerialX.readStringUntil('\n'));
+
+    if (error) {
+        data["id"] = moduleID;
+        data["error"] = error.c_str();
+        serializeJson(data, SerialX);
+        SerialX.println();
+        return;
+    }
+
+    uint8_t cmd = data["cmd"];
+    data.clear();
+
+    switch(cmd) {
+        case 1:
+            data["id"] = moduleID;
+            data["temp"] = random(0, 100);
+            serializeJson(data, SerialX);
+            SerialX.println();
+            break;
+    }
+}
 
 void setup() {
     preferences.begin("module");
-    id = preferences.getUChar("id");
-    if (id == 0) {
-        Serial.println("This device is not registered with a Module ID.");
-        exit(0);
-    }
+    moduleID = preferences.getUChar("id");
+    if (moduleID == 0) exit(0);
+
+    SerialX.begin(115200);
 }
 
 void loop() {
